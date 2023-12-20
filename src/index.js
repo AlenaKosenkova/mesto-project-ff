d82aed6d76d643900253182cb436e0d98fecfaaa
Alena Kosenkova <vyazhi.lyubi@yandex.ru>
2023-12-20 18:57:16 +0300

    Добавление картинок и шрифтов

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
-console.log('Hello, World!');
+const addIconImage = new URL('./images/add-icon.svg', import.meta.url);
+const avatarImage = new URL('./images/avatar.jpg', import.meta.url);
+const card1Image = new URL('./images/card_1.jpg', import.meta.url);
+const card2Image = new URL('./images/card_2.jpg', import.meta.url);
+const card3Image = new URL('./images/card_3.jpg', import.meta.url);
+const closeImage = new URL('./images/close.svg', import.meta.url);
+const deleteIconImage = new URL('./images/delete-icon.svg', import.meta.url);
+const editIconImage = new URL('./images/edit-icon.svg', import.meta.url);
+const likeActiveImage = new URL('./images/like-active.svg', import.meta.url);
+const likeinActiveImage = new URL('./images/like-inactive.svg', import.meta.url);
+const logoImage = new URL('./images/logo.svg', import.meta.url);
+const interBlackFont = new URL('./vendor/fonts/Inter-Black.woff2', import.meta.url);
+const interMediumFont = new URL('./vendor/fonts/Inter-Medium.woff2', import.meta.url);
+const interRegularFont = new URL('./vendor/fonts/Inter-Regular.woff2', import.meta.url);
 
-const numbers = [2, 3, 5];
+import './pages/index.css';//импорт главного файла стилей
 
-// Стрелочная функция. Не запнётся ли на ней Internet Explorer?
-const doubledNumbers = numbers.map(number => number * 2);
-
-console.log(doubledNumbers); // 4, 6, 10 
+const imagesMesto = [
+  { name: 'addIcon', link: addIconImage },
+  { name: 'avatar', link: avatarImage },
+  { name: 'card1', link: card1Image },
+  { name: 'card2', link: card2Image },
+  { name: 'card3', link: card3Image },
+  { name: 'close', link: closeImage },
+  { name: 'deleteIcon', link: deleteIconImage },
+  { name: 'editIcon', link: editIconImage },
+  { name: 'likeActive', link: likeActiveImage },
+  { name: 'likeInactive', link: likeinActiveImage },
+  { name: 'logo', link: logoImage },
+  { name: 'interBlackFont', link: interBlackFont },
+  { name: 'interMediumFont', link: interMediumFont },
+  { name: 'interRegularFont', link: interRegularFont },
+]; 
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path'); // подключаем path к конфигу вебпак
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // подключите плагин, 
+//это класс, с помощью которого можно конструировать объекты
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // подключили плагин
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
 
 module.exports = {
   entry: { 
@@ -32,9 +36,39 @@ module: {
       use: 'babel-loader',
       // исключает папку node_modules, файлы в ней обрабатывать не нужно
       exclude: '/node_modules/'
-    }
+    },
+
+    {
+      // регулярное выражение, которое ищет все файлы с такими расширениями
+      test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+      type: 'asset/resource'
+    },
+
+    {
+      // применять это правило только к CSS-файлам
+      test: /\.css$/,
+      // при обработке этих файлов нужно использовать
+      // MiniCssExtractPlugin.loader и css-loader
+      use: [MiniCssExtractPlugin.loader, {
+        loader: 'css-loader',
+        options: { 
+          importLoaders: 1 
+        },
+      },
+      'postcss-loader'
+      ]
+    },
+
     ]
-}
+},
+
+plugins: [
+  new HtmlWebpackPlugin({
+    template: './src/index.html' // путь к файлу index.html
+  }),
+  new CleanWebpackPlugin(), // использовали плагин
+  new MiniCssExtractPlugin(), // подключение плагина для объединения файлов
+]
 
 }
 
