60b89ff597e27e8f879bb785885637ff3b5cfddb
Alena Kosenkova <vyazhi.lyubi@yandex.ru>
2023-12-24 13:12:32 +0300

    Добавила гитигнор

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const addIconImage = new URL('./images/add-icon.svg', import.meta.url);
-const avatarImage = new URL('./images/avatar.jpg', import.meta.url);
 const card1Image = new URL('./images/card_1.jpg', import.meta.url);
 const card2Image = new URL('./images/card_2.jpg', import.meta.url);
 const card3Image = new URL('./images/card_3.jpg', import.meta.url);
@@ -8,16 +7,11 @@ const deleteIconImage = new URL('./images/delete-icon.svg', import.meta.url);
 const editIconImage = new URL('./images/edit-icon.svg', import.meta.url);
 const likeActiveImage = new URL('./images/like-active.svg', import.meta.url);
 const likeinActiveImage = new URL('./images/like-inactive.svg', import.meta.url);
-const logoImage = new URL('./images/logo.svg', import.meta.url);
-const interBlackFont = new URL('./vendor/fonts/Inter-Black.woff2', import.meta.url);
-const interMediumFont = new URL('./vendor/fonts/Inter-Medium.woff2', import.meta.url);
-const interRegularFont = new URL('./vendor/fonts/Inter-Regular.woff2', import.meta.url);
 
 import './pages/index.css';//импорт главного файла стилей
 
 const imagesMesto = [
   { name: 'addIcon', link: addIconImage },
-  { name: 'avatar', link: avatarImage },
   { name: 'card1', link: card1Image },
   { name: 'card2', link: card2Image },
   { name: 'card3', link: card3Image },
@@ -26,7 +20,6 @@ const imagesMesto = [
   { name: 'editIcon', link: editIconImage },
   { name: 'likeActive', link: likeActiveImage },
   { name: 'likeInactive', link: likeinActiveImage },
-  { name: 'logo', link: logoImage },
   { name: 'interBlackFont', link: interBlackFont },
   { name: 'interMediumFont', link: interMediumFont },
   { name: 'interRegularFont', link: interRegularFont },
